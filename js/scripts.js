0b97af52d0fbd1e48c9aa09dd8be8aa8ef009619
delcidj22 <jdelcdid23@gmail.com>
2021-12-11 23:11:49 -0800

    added to js, changed format, still trying to get it to work

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,33 @@ this.size = size
 this.topping = topping
 } 
 
-Pizza.prototype.cost = function() {
-  let pizzaTotal = 0;
-  const size = new Map ([
-    ["Small", 15.00],
-    ["Mediun", 19.00],
-    ["Large", 22.00],
-  ])
-  const topping = new Map ([
-    ["Pepperoni", 1.50],
-    ["Ham", 1.50],
-    ["Pineapple", 1.00],
-    ["Onion", 1.00],
-    ["Olives", 1.00],
-    ["Mixed Peppers", 1.00],
-    ["Pesto", 1.25],
-  ]);
-  this.topping.forEach(function(element) {
-    pizzaTotal += topping.get(element) 
-  });
-  total = size.get(this.size) + pizzaTotal;
-  return total;
+Pizza.prototype.totalCost = function() {
+  let toppingPrice = 0;
+  let sizePrice = 0; 
+  for(let i = 0; i <this.topping.length; i++) {
+    toppingPrice += 2;
+  };
+
+  if(this.size === 'Small') {
+    sizePrice = 15.00;
+  } else if(this.size === 'Medium') {
+    sizePrice = 19.00;
+  } else if(this.size === 'Large') {
+    sizePrice = 22.00;
+  }
+  return toppingPrice + sizePrice;
 }
 
-
 $(document).ready(function() {
-  $("#pizza").submit(function(event) {
-    event.preventDefault();
-    let size = $("input:radio[name=size]:checked").val(); 
-    let topping = $("input:radio[name=topping]:checked").val();
-    let pizzaCost = size + topping
+  $("#pizza").submit(function(e) {
+    e.preventDefault();
+    const topping = $('input[name="topping"]:checked').map(function() {
+      return $(this).val();
+    }).get();
+    const size = $("size").val();
+    const total = new Pizza(topping, size);
+    const cost = (total.totalCost());
+    $('#cost').html(price);
   });
-  pizzaCost = new Pizza("size", "topping");
-  $("#totalCost").text(pizzaCost.cost());
 });
+

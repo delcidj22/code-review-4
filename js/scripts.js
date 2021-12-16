87e38f5c1b3f22cd9299a224b34b03a28c58fab1
delcidj22 <jdelcdid23@gmail.com>
2021-12-15 18:51:40 -0800

    change input syntax to get page to push through

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,43 @@
-function Pizza(size, topping) {
-this.size = size; 
+function Pizza(topping, size) {
 this.topping = topping;
+this.size = size; 
+
 } 
+
 
 Pizza.prototype.totalCost = function() {
   let toppingPrice = 0;
   let sizePrice = 0; 
   for(let i = 0; i <this.topping.length; i++ ) {
-    toppingPrice++;
+    toppingPrice += 2;
   };
 
-  if(this.size === 'pizzaSize1') {
+  if(this.size === "small") {
     sizePrice = 15.00;
-  } else if(this.size === 'pizzaSize2') {
+  } 
+  else if(this.size === "medium") {
     sizePrice = 19.00;
-  } else if(this.size === 'pizzaSize3') {
+  } 
+  else if(this.size === "large") {
     sizePrice = 22.00;
-  }
-  return toppingPrice + sizePrice;
+  }  
+  return sizePrice + toppingPrice;
 }
 
+
+
 $(document).ready(function() {
-  $("#pizza-order").submit(function(e) {
-    e.preventDefault();
-    const topping = $('input[name="topping"]:checked').map(function() {
+  $("#pizza-order").submit(function(event) {
+    event.preventDefault();
+    const topping = $('input:checkbox[name="topping"]:checked').map(function() {
       return $(this).val();
     }).get();
-    const size = $("#size").val();
+    const size = $("input:radio[name=size]:checked").val()
     const total = new Pizza(topping, size);
     const price = (total.totalCost());
     $('#cost').html(price);
     $('#orderComplete').show();
-
+    console.log(total)
   });
 });
 
